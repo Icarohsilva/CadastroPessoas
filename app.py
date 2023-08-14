61416ebd9b223f4d2b08e9fda7b7d7839e49ef94
IcaroSilva <icaro_henrique18@hotmail.com>
2023-08-14 09:41:16 -0300

    ajustes

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 # Sistema de Cadastro e Gerenciamento de Usuários 
 import base64
-import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt  # Se não estiver usando, você pode remover essa linha
 import webbrowser 
 import random
 import string
@@ -12,11 +12,7 @@ from wtforms import StringField, PasswordField, BooleanField, SubmitField, Integ
 from wtforms.validators import InputRequired, Length, Email, EqualTo, ValidationError,DataRequired
 from werkzeug.security import generate_password_hash
 from flask_mail import Message, Mail
-from forms import CadastroForm, EnderecoForm, SenhaForm  # Importe os formulários corretamente
-from reportlab.lib.pagesizes import letter
-from reportlab.pdfgen import canvas
-from sqlalchemy import func
-from io import BytesIO
+from forms import CadastroForm, EnderecoForm, SenhaForm
 
 # Configurar o Flask
 app = Flask(__name__)
